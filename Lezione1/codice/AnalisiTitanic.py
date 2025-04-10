5543d9e5399d5433f20207fc9e41b8f511e8fe38
GimmiPrud <prudenzig@gmail.com>
2025-04-10 10:50:43 +0200

    ...

--- Lezione1/codice/AnalisiTitanic.py
+++ Lezione1/codice/AnalisiTitanic.py
@@ -1,8 +1,12 @@
 
 import pandas as pd
 
-DataTitanic = pd.read_csv("dati/TitanicData.csv")
-DataTitanic.head(10)
+DataTitanic = pd.read_csv("..dati/TitanicData.csv")
+print(DataTitanic.tail(7))
+print("\n")
+
 ages = DataTitanic["Age"]
+print(ages.describe())
+print("\n")
 
 
