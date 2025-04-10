faa56523cf72b36160017617b712d314087fc0e7
GimmiPrud <prudenzig@gmail.com>
2025-04-10 10:24:31 +0200

    ..

---
+++ Lezione1/codice/provePandas.py
@@ -0,0 +1,15 @@
+
+dict = {
+    "name":[
+        "Nicolò Campanelli",
+        "Giacomo Panella",
+        "Federico Belleggi",
+        "Edoardo Anselmi",
+        "Mattia Campanelli",
+        "Alessandra Tarmati",
+        "Elena Fabbretti",
+    ],
+    "age":[26,34,27,26,28,29,26],
+    "sex":["male","male","male","male","male","female","female"]
+}
+
